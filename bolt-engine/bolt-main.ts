87bfef9218af2686724e70175c446adfdf98b56a
snowStorrm <ryanjz101@gmail.com>
2025-10-22 15:58:20 -0400

    added a Color class and clearCanvas() func

--- bolt-engine/bolt-main.ts
+++ bolt-engine/bolt-main.ts
@@ -1,9 +1,9 @@
+import { transformColorToRGBA } from "./bolt-boilerplate";
+
 export var boltGL: WebGL2RenderingContext | null;
 export var boltCanvas: HTMLCanvasElement; 
-
-let setup = false;
-
 export var BUFFER_TYPE_INT: any;
+
 export function createCanvas(canvas?: HTMLCanvasElement, width?: number, height?: number) {
     boltCanvas = canvas || document.createElement("canvas");
     boltCanvas.width = width || 640;
@@ -42,3 +42,45 @@ export function createCanvas(canvas?: HTMLCanvasElement, width?: number, height?
     ]
 }
 
+export function clearCanvas(col?: Color) {
+    let clearCol = (col) ? col.glCol : [0, 0, 0, 1];
+    (boltGL!.clearColor as any)(...clearCol);
+    boltGL!.clear(boltGL!.COLOR_BUFFER_BIT);
+}
+
+export enum COLOR_TYPE{LA, RGBA, CMYKA, HSVA};
+export class Color {
+    type: COLOR_TYPE;
+    rawCol: number[];
+    glCol: number[];
+    constructor(type: COLOR_TYPE.LA, lightness: number, alpha: number);
+    constructor(type: COLOR_TYPE.RGBA, red: number, green: number, blue: number, alpha: number);
+    constructor(type: COLOR_TYPE.CMYKA, cyan: number, magenta: number, yellow: number, key: number, alpha: number);
+    constructor(type: COLOR_TYPE.HSVA, hue: number, saturation: number, brightness: number, alpha: number);
+    constructor(type: COLOR_TYPE, c1: number, c2: number, c3?: number, c4?: number, c5?: number) {
+        this.type = type;
+        switch (type) {
+            case COLOR_TYPE.LA: this.rawCol = [c1, c2]; break;
+            case COLOR_TYPE.RGBA: this.rawCol = [c1, c2, c3!, c4!]; break;
+            case COLOR_TYPE.CMYKA: this.rawCol = [c1, c2, c3!, c4!, c5!]; break;
+            case COLOR_TYPE.HSVA: this.rawCol = [c1, c2, c3!, c4!]; break;
+            default: throw new Error("Invalid color type selected. Use only those found in Bolt.COLOR_TYPE.");
+        }
+        this.glCol = transformColorToRGBA(this.type, this.rawCol);
+    }
+    set(type: COLOR_TYPE.LA, lightness: number, alpha: number): void;
+    set(type: COLOR_TYPE.RGBA, red: number, green: number, blue: number, alpha: number): void;
+    set(type: COLOR_TYPE.CMYKA, cyan: number, magenta: number, yellow: number, key: number, alpha: number): void;
+    set(type: COLOR_TYPE.HSVA, hue: number, saturation: number, brightness: number, alpha: number): void;
+    set(type: COLOR_TYPE, c1: number, c2: number, c3?: number, c4?: number, c5?: number) {
+        this.type = type;
+        switch (type) {
+            case COLOR_TYPE.LA: this.rawCol = [c1, c2]; break;
+            case COLOR_TYPE.RGBA: this.rawCol = [c1, c2, c3!, c4!]; break;
+            case COLOR_TYPE.CMYKA: this.rawCol = [c1, c2, c3!, c4!, c5!]; break;
+            case COLOR_TYPE.HSVA: this.rawCol = [c1, c2, c3!, c4!]; break;
+            default: throw new Error("Invalid color type selected. Use only those found in Bolt.COLOR_TYPE.");
+        }
+        this.glCol = transformColorToRGBA(this.type, this.rawCol);
+    }
+}
--- bolt-engine/bolt.ts
+++ bolt-engine/bolt.ts
@@ -1,5 +1,5 @@
-import { createCanvas } from './bolt-main'
+import { createCanvas, clearCanvas, Color, COLOR_TYPE } from './bolt-main'
 import { Shader } from './bolt-shader'
 import { Buffer, BUFFER_TYPE } from './bolt-buffer'
 
-export { createCanvas, Shader, Buffer, BUFFER_TYPE }
+export { createCanvas, clearCanvas, Color, COLOR_TYPE, Shader, Buffer, BUFFER_TYPE }
